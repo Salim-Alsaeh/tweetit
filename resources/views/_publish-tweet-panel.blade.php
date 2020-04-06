753f5c44f446a23e5f31604c3f308a25564dfe9d
Salim-Alsaeh <salem.ali.alsaeh@gmail.com>
2020-04-06 07:30:44 +0200

    styling homepage

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,18 @@
+<div class="border border-blue-400 rounded-lg py-6 px-8 p-8 mb-8">
+    <form action="">
+                    <textarea
+                        name="body"
+                        class="w-full"
+                        placeholder="What's' up doc?"
+                    >
+                    </textarea>
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+                <img class="rounded-full mr-2" src="https://i.pravatar.cc/40" alt="profile pic">
+            <button class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white"  type="submit">Prrrrrrrr!</button>
+
+        </footer>
+
+    </form>
+</div>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,13 @@
+<div class="flex p-4 border-b border-b-gray-400 ">
+    <div class="mr-2 flex-shrink-0">
+        <img class="rounded-full mr-2" src="https://i.pravatar.cc/50" alt="profile pic">
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+
+        <p class="text-sm">
+            Laravel is the best framework ever made wallahi Laravel is the best framework ever made wallahi Laravel is the best framework ever made wallahi Laravel is the best framework ever made wallahi
+        </p>
+    </div>
+</div>
